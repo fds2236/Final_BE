951a5c889213c46274bfeabd4ed2438972c447ee
doyeonS2 <1162004@naver.com>
2022-11-28 14:25:13 +0900

    도연/11.28/Entity수정

--- src/main/java/com/mute/Final_BE/dto/MemberDTO.java
+++ src/main/java/com/mute/Final_BE/dto/MemberDTO.java
@@ -6,14 +6,13 @@ import java.util.Date;
 // 도연 작업중 (수빈, 찬호 확인해주세요)
 @Getter @Setter
 public class MemberDTO {
-    private Long user_num; // 회원번호
-    private String user_id; // 회원아이디
+    private Long userNum; // 회원번호
+    private String userId; // 회원아이디
     private String pwd; // 비밀번호
     private String name; // 이름
     private String phone; // 전화번호
     private String mail; // 메일
     private String addr; // 주소
-    private Date reg_Date; // 가입일
-    private Date unreg_Date; // 탈퇴일
-    private String is_unreg; // 탈퇴여부
+    private Date regDate; // 가입일
+    private Date unRegDate; // 탈퇴일
 }
