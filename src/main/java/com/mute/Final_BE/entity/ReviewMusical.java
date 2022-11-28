c738b1bf1dc69e78b13f0028dc4c194088fd25d5
YunJeong <thkdthkd@gmail.com>
2022-11-29 00:25:01 +0900

    윤정/11.28/엔티티수정

--- src/main/java/com/mute/Final_BE/entity/ReviewMusical.java
+++ src/main/java/com/mute/Final_BE/entity/ReviewMusical.java
@@ -3,6 +3,7 @@ package com.mute.Final_BE.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -12,10 +13,21 @@ public class ReviewMusical {
     @GeneratedValue
     private Long reviewMuId; // 뮤지컬후기 글 번호
 
-//    @ManyToOne
-//    @JoinColumn(name = "musical_id")
-//    private
+    @ManyToOne
+    @JoinColumn(name = "musical_id")
+    private Musical musical; // 공연 ID
 
-//    @OneToOne
-//    @JoinColumn(name = "user_num")
+    @OneToOne
+    @JoinColumn(name = "user_num")
+    private Member member; // 회원번호
+
+    private LocalDateTime writeDate; // 작성일
+    private int scoreStory; // 스토리 별점
+    private int scoreDirect; // 연출 별점
+    private int scoreCast; // 캐스팅 별점
+    private int scoreNumber; // 넘버 별점
+
+    @Lob
+    private String reviewMuTxt; // 뮤지컬 후기 텍스트
+
 }
--- src/main/java/com/mute/Final_BE/entity/ReviewSeat.java
+++ src/main/java/com/mute/Final_BE/entity/ReviewSeat.java
@@ -2,10 +2,8 @@ package com.mute.Final_BE.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -14,4 +12,26 @@ public class ReviewSeat {
     @Id
     @GeneratedValue
     private Long reviewSeId; // 좌석후기 글 번호
+
+    @OneToOne
+    @JoinColumn(name = "musical_id")
+    private Musical musical; // 공연 ID
+
+    @OneToOne
+    @JoinColumn(name = "theater_id")
+    private Theater theater; // 공연장 ID
+
+    @OneToOne
+    @JoinColumn(name = "user_num")
+    private Member member; // 회원 번호
+
+    private int seatNum; // 좌석번호
+    private LocalDateTime writeDate; // 작성일
+    private int scoreSeat; // 좌석 별점
+    private int scoreView; // 시야 별점
+    private int scoreSound; // 음향 별점
+    private int scoreLight; // 조명 별점
+
+    @Lob
+    private String reviewSeTxt; // 좌석 후기 내용
 }
